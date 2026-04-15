dbe9757a4dfb554bc9295191e24bcdefd5fe7eea
pliniooop <pedroservulo980@gmail.com>
2026-04-15 14:16:26 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,12 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        int i = 100;
+
+        while (i <= 200) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
