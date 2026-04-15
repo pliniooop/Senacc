29147da4dabaa9de6b734c7db268e697352c5d1b
pliniooop <pedroservulo980@gmail.com>
2026-04-15 15:13:01 -0300

    Rename class Exercicio3 to Exercicio03

--- Exercicio03.java
+++ Exercicio03.java
@@ -1,7 +1,7 @@
 Pedro Henrique de Carvalho Servulo
 import java.util.Scanner;
 
-public class Exercicio3 {
+public class Exercicio03 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
