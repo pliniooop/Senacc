11310799a3bebf737c1e3c5e07fc47e4a6de305b
pliniooop <pedroservulo980@gmail.com>
2026-04-15 14:54:12 -0300

    Add files via upload

---
+++ Exercicio12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio12 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        System.out.print("Sequência: ");
+
+        for (int i = 1; i <= numero; i++) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
