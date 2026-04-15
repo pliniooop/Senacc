d766e4b05ff72045edd48acdd57828ce73e8224d
pliniooop <pedroservulo980@gmail.com>
2026-04-15 14:51:55 -0300

    Add files via upload

---
+++ Exercicio11.java
@@ -0,0 +1,10 @@
+public class Exercicio11 {
+    public static void main(String[] args) {
+
+        for (int i = 15; i <= 100; i++) {
+            int quadrado = i * i;
+            System.out.println("O quadrado de " + i + " é " + quadrado);
+        }
+
+    }
+}
