b15071686809422c3ba5e2fb9f47b365949dc353
pliniooop <pedroservulo980@gmail.com>
2026-04-15 14:26:34 -0300

    Add files via upload

---
+++ Exercicio4.java
@@ -0,0 +1,13 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        while (numero <= 20) {
+            double metade = numero / 2.0;
+            System.out.println("A metade de " + numero + " é " + metade);
+            numero++;
+        }
+
+    }
+}
