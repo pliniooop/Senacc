2c3b8dd00b9a7766ddc60e684717287ffae204e3
pliniooop <pedroservulo980@gmail.com>
2026-04-15 15:15:20 -0300

    Update and rename Exercicio9.java to Exercicio09.java

--- Exercicio09.java
+++ Exercicio09.java
@@ -1,7 +1,7 @@
 Pedro Henrique de Carvalho Servulo
 import java.util.Scanner;
 
-public class Exercicio9 {
+public class Exercicio09 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
