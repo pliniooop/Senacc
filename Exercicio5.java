d288fd5d1801158aee065265772aa521503b6e99
pliniooop <pedroservulo980@gmail.com>
2026-04-15 14:28:56 -0300

    Add files via upload

---
+++ Exercicio5.java
@@ -0,0 +1,12 @@
+public class Exercicio5 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+            i++;
+        }
+
+    }
+}
