f4bb497ed7756b3a910ae67abaa83555217a8835
pliniooop <pedroservulo980@gmail.com>
2026-04-15 14:37:55 -0300

    Add files via upload

---
+++ Exercicio8.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int aluno = 1;
+
+        while (aluno <= 5) {
+            double nota1, nota2;
+
+            // validação da primeira nota
+            do {
+                System.out.print("Digite a 1ª nota do aluno " + aluno + ": ");
+                nota1 = sc.nextDouble();
+
+                if (nota1 < 0 || nota1 > 10) {
+                    System.out.println("Nota inválida! Digite entre 0 e 10.");
+                }
+
+            } while (nota1 < 0 || nota1 > 10);
+
+            // validação da segunda nota
+            do {
+                System.out.print("Digite a 2ª nota do aluno " + aluno + ": ");
+                nota2 = sc.nextDouble();
+
+                if (nota2 < 0 || nota2 > 10) {
+                    System.out.println("Nota inválida! Digite entre 0 e 10.");
+                }
+
+            } while (nota2 < 0 || nota2 > 10);
+
+            double media = (nota1 + nota2) / 2;
+
+            System.out.println("Média do aluno " + aluno + ": " + media);
+            System.out.println("---------------------------");
+
+            aluno++;
+        }
+
+        sc.close();
+    }
+}
